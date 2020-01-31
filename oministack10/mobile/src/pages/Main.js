8d44296994f146c859a429f91c528e7096df46f9
Francisco Souza <francisco.souza-sys@parceiros-kroton.com.br>
2020-01-31 10:32:14 -0300

    mobile

--- oministack10/mobile/src/pages/Main.js
+++ oministack10/mobile/src/pages/Main.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react'
-import { StyleSheet } from 'react-native'
-import MapView from 'react-native-maps'
+import { StyleSheet, Image, View, Text } from 'react-native'
+import MapView, { Marker, Callout } from 'react-native-maps'
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
 
-function Main() {
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null) 
     useEffect(() => {
         async function loadInitialPosition(){
@@ -32,13 +32,48 @@ function Main() {
         return null
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map} />
+    return <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{latitude: -27.2111164, longitude: -49.6374491}}>
+            <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/29492106?s=460&v=4'}} />
+
+            <Callout onPress={() => {
+                navigation.navigate('Profile', { github_username: 'chico2501' })
+            }}>
+                <View style={styles.callout}>
+                    <Text style={styles.devName}>Francisco</Text>
+                    <Text style={styles.devBio}>Bio</Text>
+                    <Text style={styles.devTechs}>React</Text>
+                </View>
+            </Callout>
+        </Marker>
+    </MapView>
 }
 
 const styles = StyleSheet.create({
     map:{
         flex: 1
     },
+    avatar:{
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF'
+    },
+    callout:{
+        width: 260,
+    },
+    devName:{
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    devBio:{
+        color: '#666',
+        marginTop: 5,
+    },
+    devTechs:{
+        marginTop: 5,
+    },  
 })
 
 export default Main
